0df94ec0c357f8387f3f706c474929d7de0a8b51
Shouko <45937104+Ringooh@users.noreply.github.com>
2021-08-12 21:25:48 +0300

    Add files via upload

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,9 +1,1 @@
-$(function(){
-  $('.nav-bar-toggle').on('click', function(){
-    if ($('.nav-bar-links').hasClass('active')){
-      $('.nav-bar-links').removeClass('active');
-    } else {
-      $('.nav-bar-links').addClass('active');
-    }
-  })
-});
+$(function(){$('.nav-bar-toggle').on('click',function(){if($('.nav-bar-links').hasClass('active')){$('.nav-bar-links').removeClass('active');}else{$('.nav-bar-links').addClass('active');}})});
